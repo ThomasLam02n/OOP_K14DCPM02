68a2b2d45d5fe427773583b7666858a03de96b95
ThomasLam02n <87083246+ThomasLam02n@users.noreply.github.com>
2022-01-12 14:27:32 +0700

    doi ten oop

--- src/tuan1/object_oriented/OOTestDrive.java
+++ src/tuan1/object_oriented/OOTestDrive.java
@@ -1,6 +1,6 @@
 package tuan1.object_oriented;
 
-public class object_oriented_TestDrive {
+public class OOTestDrive {
     public static void main(String[] args) {
          //Khai báo biến "account", dùng toán tử "new" tạo đối tượng Account trong vùng nhớ Heap, Liên kết biến "account" tới đối trượng trong vùng nhớ heap bằng toán tử gán " = "
         Account account = new Account();
--- src/tuan1/object_oriented/object_oriented.java
+++
@@ -1,5 +0,0 @@
-package tuan1.object_oriented;
-
-public class object_oriented { //Lập trình hướng đối tượng
-    
-}
