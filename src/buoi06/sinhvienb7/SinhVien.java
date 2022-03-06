546b4e9b91cf26ac22e40c43ff22da8c0e5f6bc4
lamvuthanhtai02 <you@example.com>
2022-03-06 20:10:33 +0700

    finish sinhvienb7 homework

--- src/buoi06/sinhvienb7/SinhVien.java
+++ src/buoi06/sinhvienb7/SinhVien.java
@@ -1,5 +1,78 @@
 package buoi06.sinhvienb7;
 
+import java.util.Scanner;
+
 public class SinhVien {
+    private int mssv;
+    private String name;
+    private String address;
+    private long phoneNumber;
+    Scanner sc = new Scanner(System.in);
+    Scanner scs = new Scanner(System.in);
+    SinhVien(){
+
+    }
+
+    SinhVien(int mssv, String name, String address, long phoneNumber){
+        this.mssv = mssv;
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
     
+    public void setMssv(int mssv) {
+        this.mssv = mssv;
+    }
+
+    public int getMssv() {
+        return mssv;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        String stc = "MSSV: " +this.getMssv() + "Name: " +this.getName() +"Address: "+this.getAddress();
+        stc += "PhoneNumber: "+this.getPhoneNumber();
+        return super.toString();
+    }
+
+    public void input(){
+        System.out.println("MSSV: ");
+        this.mssv = scs.nextInt();
+        System.out.println("Name: ");
+        this.name = sc.nextLine();
+        System.out.println("Address: ");
+        this.address = sc.nextLine();
+        System.out.println("Phone: ");
+        this.phoneNumber = scs.nextLong();
+    }
+
+    public void output(){
+        System.out.print("MSSV: "+this.mssv);
+        System.out.print("  Name: "+this.name);
+        System.out.print("  Address: "+this.address);
+        System.out.println("  Phone: "+this.phoneNumber);    }
 }
--- src/buoi06/sinhvienb7/SinhVienTestDrive.java
+++ src/buoi06/sinhvienb7/SinhVienTestDrive.java
@@ -1,5 +1,51 @@
 package buoi06.sinhvienb7;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 public class SinhVienTestDrive {
-    
+    public static void main(String[] args) {
+
+    //Add Sinh viên vào Array
+        int n;
+        Scanner sc = new Scanner(System.in);
+            System.out.println("So luong SV can nhap: ");
+            n = sc.nextInt();
+
+        SinhVien[] dssv = new SinhVien[n];
+        for(int i = 0;i < dssv.length;i++){
+            dssv[i] = new SinhVien();
+            System.out.printf("Nhap thong tin SV thu %d:\n", i+1);
+            dssv[i].input();
+        }
+        System.out.println("Danh Sach SV:");
+        for(int i = 0;i < dssv.length;i++){
+            
+            dssv[i].output();
+        }
+
+
+
+    //Sắp xếp theo giá trị tăng dần
+        List<SinhVien> list = Arrays.asList(dssv);
+        Comparator<SinhVien> comparator = new Comparator<SinhVien>() {
+
+            @Override
+            public int compare(SinhVien o1, SinhVien o2) {
+                // TODO Auto-generated method stub
+                return Integer.compare(o1.getMssv(), o2.getMssv());
+            }    
+        };
+        list.sort(comparator);
+
+        System.out.println("Sau khi sap xep: ");
+
+        for(int i = 0;i < dssv.length;i++){
+            
+            dssv[i].output();
+        }
+        sc.close();
+    }
 }
